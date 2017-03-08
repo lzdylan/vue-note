608c616f03a8c8018e66f56ca55c4e728220c1d6
lzdylan <923060635@qq.com>
2017-03-08 08:33:16 +0800

    vue note

--- 表单控件绑定.js
+++ 表单控件绑定.js
@@ -7,8 +7,11 @@
  2、Checkbox数据双向绑定：如果没有设置value则会选择所有的CheckBox，因为CheckBox的默认值是on
  3、Radio数据双向绑定：Radio有会将相同name属性的Radio分为一组
  4、Select数据双向绑定
- 注：表单控件中，text 类型的数据双向绑定所涉及的表单控件如 text、number、range……等都是通过 value 来传递控件所包含的信息，双向绑定所绑定的其实就是控件的 value 属性。
- 而 checkbox、radio 以及 select 就不同了，就控件而言，checkbox 和 radio 控件通过 checked 属性来表示控件的状态，select 控件通过 option 子元素的 selected 属性来表示控件的状态，从表单层面来讲，需要结合 value 和 checked / selected 属性才能获取这三种表单控件的完整状态，也就是说，value 属性是这三种表单控件状态的一部分。
+ 注：表单控件中，text 类型的数据双向绑定所涉及的表单控件如 text、number、range……等都是通过 value 来传递控件所包含的信息，
+ 双向绑定所绑定的其实就是控件的 value 属性。
+ 而 checkbox、radio 以及 select 就不同了，就控件而言，checkbox 和 radio 控件通过 checked 属性来表示控件的状态，select 控件
+ 通过 option 子元素的 selected 属性来表示控件的状态，从表单层面来讲，需要结合 value 和 checked / selected 属性才能获取这三
+ 种表单控件的完整状态，也就是说，value 属性是这三种表单控件状态的一部分。
  ----------------------------------------------------------------------------------------------------------------------
  绑定value：
  单个CheckBox绑定：   <input type="checkbox" v-model="toggle" v-bind:true-value="a" v-bind:true-value="b"/>
